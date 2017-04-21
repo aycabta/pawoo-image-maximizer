409e50490261523fecde9f9f5ad1a9dd938bc658
Code Ass <aycabta@gmail.com>
2017-04-21 18:25:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Pawoo Image Maximizer
+=====================
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/Pawoo_Image_Maximizer)
---
+++ pawoo-image-maximizer.user.js
@@ -0,0 +1,46 @@
+﻿// ==UserScript==
+// @name       Pawoo Image Maximizer
+// @namespace  http://aycabta.github.io/
+// @version    0.0.1
+// @description  Pawoooooooooo
+// @include    /^https:\/\/pawoo.net/
+// @copyright  2017+, You
+// ==/UserScript==
+
+(function() {
+    var sheet;
+    var style = document.createElement('style');
+    style.setAttribute('id', 'great-css');
+    style.type = "text/css";
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(style);
+    sheet = style.sheet;
+    sheet.insertRule('.media-spoiler {' +
+                     '    display: none !important;' +
+                     '}', sheet.cssRules.length);
+    sheet.insertRule('.media-item .u-photo {' +
+                     '    background-image: none !important;' +
+                     '}', sheet.cssRules.length);
+    sheet.insertRule('.status__attachments__inner {' +
+                     '    height: auto !important;' +
+                     '    display: block !important;' +
+                     '}', sheet.cssRules.length);
+    var maximize = function(doc) {
+        var photos = doc.querySelectorAll('.media-item .u-photo');
+        for (var i = 0; i < photos.length; i++) {
+            var photo = photos[i];
+            var url = photo.attributes['href'].value;
+            var img = document.createElement("img");
+            img.src = url;
+            img.style.width = '100%';
+            photo.appendChild(img);
+        }
+    }
+    maximize(document);
+    document.body.addEventListener(
+        'AutoPagerize_DOMNodeInserted',
+        function(e) {
+            maximize(e.target);
+        },
+        false);
+})();
